e66d5e2dbc1f4235f4a41151a1209f8d831ee255
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-28 14:24:12 -0800

    Added map:

--- starter/script.js
+++ starter/script.js
@@ -16,7 +16,19 @@ if (navigator.geolocation) {
     position => {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(longitude);
+      const coord = [latitude, longitude];
+
+      const map = L.map('map').setView(coord, 13);
+      console.log(L);
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     () => {
       alert(`could not get your position`);

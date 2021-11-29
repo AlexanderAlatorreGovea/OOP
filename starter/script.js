37d1ced950c93b2510066317e1fc711d991c8a50
Alexander <Alexander_alatorre1993@hotmail.com>
2021-11-28 17:31:04 -0800

    added new methods and classes

--- starter/script.js
+++ starter/script.js
@@ -11,27 +11,133 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coord = [latitude, longitude];
+let map;
+let mapEvent;
+let LeafletMap;
 
-      const map = L.map('map').setView(coord, 13);
-      console.log(L);
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+class App {
+  #map;
+  #mapEvent;
+  #LeafletMap;
+
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+        alert(`could not get your position`);
+      });
+    }
+  }
+
+  _loadMap(position) {
+    this.#LeafletMap = L;
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coord = [latitude, longitude];
+
+    this.#map = this.#LeafletMap.map('map').setView(coord, 13);
+
+    this.#LeafletMap
+      .tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+      })
+      .addTo(this.#map);
 
-      L.marker([51.5, -0.09])
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-    },
-    () => {
-      alert(`could not get your position`);
-    }
-  );
+    this.#LeafletMap
+      .marker([51.5, -0.09])
+      .addTo(this.#map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputDuration.value = '';
+    inputElevation.value = '';
+
+    this.#LeafletMap
+      .marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        this.#LeafletMap.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnclick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
+
+class Workout {
+  date = new Date();
+  id = `${Date.now()}`.slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const app = new App();
+const run = new Running([39, -12], 4.2, 25, 193);
+const cycling = new Cycling([39, -12], 29, 90, 193);
+console.log(cycling);
+console.log(run);

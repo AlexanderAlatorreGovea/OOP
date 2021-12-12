05ff088f2446f9773bf1186be41a24b1741904b7
Alexander <Alexander_alatorre1993@hotmail.com>
2021-12-11 21:59:40 -0800

    added new builder class

---
+++ design-patterns/Factory-Pattern/User.ts
@@ -0,0 +1,50 @@
+class Address {
+  zip: string;
+  street: string;
+  constructor(zip, street) {
+    this.zip = zip;
+    this.street = street;
+  }
+}
+
+class User {
+  name: string;
+  age: number;
+  phone: number;
+  address: string;
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class UserBuilder {
+  user: User;
+  constructor(name) {
+    this.user = new User(name);
+  }
+
+  setAge(age) {
+    this.user.age = age;
+    return this;
+  }
+
+  setPhone(phone) {
+    this.user.phone = phone;
+    return this;
+  }
+
+  setAddress(address) {
+    this.user.address = address;
+    return this;
+  }
+
+  build() {
+    return this.user;
+  }
+}
+
+const user = new UserBuilder("Bob")
+  .setAge(10)
+  .setPhone(1231123)
+  .setAddress("5235 Road")
+  .build();
